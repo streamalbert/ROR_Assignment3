767038451e11974bbbc35e91a2ea495a7b8f4e67
Weichuan Tian <stream.albert@gmail.com>
2016-07-02 01:06:48 -0700

    10 links

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,33 @@
 class StaticPagesController < ApplicationController
+
   def home
   end
 
   def about
   end
+
+  def contact
+  end
+
+  def terms
+  end
+
+  def portfolio
+  end
+
+  def experience
+  end
+
+  def facebook
+  end
+
+  def linkedin
+  end
+
+  def resume
+  end
+
+  def portrait
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'about' => 'static_pages#about'
-
+  get 'contact' => 'static_pages#contact'
+  get 'terms' => 'static_pages#terms'
+  get 'portfolio' => 'static_pages#portfolio'
+  get 'experience' => 'static_pages#experience'
+  get 'facebook' => 'static_pages#facebook'
+  get 'linkedin' => 'static_pages#linkedin'
+  get 'resume' => 'static_pages#resume'
+  get 'portrait' => 'static_pages#portrait'
   root 'static_pages#home'
 end
