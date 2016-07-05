d74b841dfb3739738df94e7c511683bb1eef86be
Weichuan Tian <stream.albert@gmail.com>
2016-07-04 18:53:42 -0700

    add 10 pages and navigations

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,30 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def converter
+  def contact
+  end
+
+  def about
+  end
+
+  def terms
+  end
+
+  def demo1
+  end
+
+  def demo2
+  end
+
+  def demo3
+  end
+
+  def demo4
+  end
+
+  def facebook
+  end
+
+  def linkedIn
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'converter' => 'static_pages#converter'
-
   root 'static_pages#home'
+  get 'contact' => 'static_pages#contact'
+  get 'about' => 'static_pages#about'
+  get 'terms' => 'static_pages#terms'
+  get 'demo1' => 'static_pages#demo1'
+  get 'demo2' => 'static_pages#demo2'
+  get 'demo3' => 'static_pages#demo3'
+  get 'demo4' => 'static_pages#demo4'
+  get 'facebook' => 'static_pages#facebook'
+  get 'linkedIn' => 'static_pages#linkedIn'
 end
